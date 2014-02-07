c0a1ead63695c4eea4a89025bc002b08bcde77e3
Sam Yerkes <your.samuelyerkes@gmail.com>
2014-02-06 22:32:40 -0500

    Changed to compact output style

--- config.rb
+++ config.rb
@@ -8,7 +8,7 @@ images_dir = "assets/imgs"
 javascripts_dir = "assets/js"
 fonts_dir = "assets/fonts"
 
-output_style = :compressed
+output_style = :compact
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
